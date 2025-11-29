2848b93a4d2550b6ba7dda6ba827be9f59f22f1e
LogicPhantomX <144667317+LogicPhantomX@users.noreply.github.com>
2025-11-29 03:08:09 +0100

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,3 +0,0 @@
-// Intentionally minimal; kept for future DB switch.
-// For JSON storage we don't need a DB connection here.
-module.exports = {};
--- backend/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-
-const router = require('express').Router();
-const auth = require('../middlewares/auth');
-const dash = require('../controllers/dashboardController');
-
-router.get('/results', auth, dash.myResults);
-
-module.exports = router;
